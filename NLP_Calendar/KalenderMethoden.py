5bb8b8689e4c7c26ed318a2e5a5f84cd6e85e549
Minh <minhthuan@hotmail.de>
2021-05-13 16:14:31 +0200

    Termin löschen Methode

--- NLP_Calendar/KalenderMethoden.py
+++ NLP_Calendar/KalenderMethoden.py
@@ -49,24 +49,44 @@ def terminAnlegen(jahr, monat, tag, startStunde, startMinute, endStunde, endMinu
     ],
   },
 }
-    id = goc.getId("TestKalender")
-    event = goc.service.events().insert(calendarId=id, body=event).execute()
+    id = gac.getId("TestKalender")
+    event = gac.service.events().insert(calendarId=id, body=event).execute()
     print ('Event created: %s' % (event.get('htmlLink')))
 
 
 def terminanzeigen(jahr, monat, tag,):
   """Zum anzeigen aller Termine an dem jeweiligen Tag (Minh) """
-  events = goc.service.events().list(calendarId= goc.getId('TestKalender')).execute()
+  events = gac.service.events().list(calendarId= gac.getId('TestKalender')).execute()
   datum = str(datetime.date(jahr,monat,tag))
+  print(events['items'][0]['id'])
+  print("__________")
   print("Die Termine für den", datum, ":")
   for event in events['items']:
      if datum in event.get('start')['dateTime']: 
       print( event.get('summary')+ " um " + event.get('start')['dateTime'])
   
-
+terminanzeigen(2021,5,13)
 def terminBearbeiten():
   pass
-def terminloeschen():
-  pass
+def terminloeschen(jahr,monat,tag,startStunde,startMinute):
+  """Zum löschen eines Termin anhand des Datum und AnfangZeitpunkt (Minh) """
+  events = gac.service.events().list(calendarId= gac.getId('TestKalender')).execute()
+  print("die events", events['items'][4]['id'])
+  startZeit = str(datetime.datetime(jahr, monat, tag, startStunde, startMinute)).replace(" ","T")
+  print(startZeit)
+  for event in events['items']:
+    print(event['start'])
+    if startZeit in event.get('start')['dateTime']:
+      terminId = event.get('id')
+      print("TerminID: ",terminId)
+      gac.service.events().delete(calendarId= gac.getId('TestKalender'), eventId= terminId).execute()
+      print("Termin wurde gelöscht!")
+    else: 
+      print("Etwas ist schief gelaufen, Termin nicht vorhanden/gefunden")
+      break
+
+#terminAnlegen(2021,5,14,16,0,18,0,'es geht','ez')
+terminloeschen(2021,5,14,16,0)
+
 
 #deleteKalender("jaEndlich")

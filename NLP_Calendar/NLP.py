d1d8e142c39e831e6627d698ca35055545a5fc6f
Minh <Minh-Huynh@outlook.de>
2021-05-25 17:13:49 +0200

    Methode für titel

--- NLP_Calendar/NLP.py
+++ NLP_Calendar/NLP.py
@@ -114,8 +114,28 @@ def getLocation():
             
 
 def getTitel():
-    
-    pass
+    # Rule based wo man den alles nach "Mit dem Titel" als Titel nimmt, evtl Intent am Ende entfernen
+    # wenn kein titel gefunden, dann nochmal nachfragen was titel sein soll und das dann nutzen
+    # PHRASE Matcher anschauen, das wahrscheinlich sinnvoller
+
+    matcher=PhraseMatcher(nlp.vocab)
+    term = ["titel"]
+    patterns = [nlp.make_doc(text) for text in term]
+    print(patterns)
+    matcher.add("titel", patterns)
+    titel = None
+    doc = nlp("Erstelle ein Termin um 15 Uhr mit dem x ich muss kacken")
+    matches = matcher(doc)
+    print(matches)
+
+    for match_id, start, end in matches:
+        titel = doc[end:len(doc)]
+        print(titel.text)
+
+    if not matches: 
+        print("Keinen Titel gefunden was wollen Sie als Titel haben?")
+        #input bla bla into Titel
+
 
  #UserEvent checkt welche Infos vom User schon gegeben wurden           
 userEvent={
---
+++ NLP_Calendar/TestUmgebung.py
@@ -0,0 +1,32 @@
+from numpy import empty
+import spacy
+from spacy.matcher import PhraseMatcher
+
+nlp=spacy.load("de_core_news_sm")
+
+
+matcher=PhraseMatcher(nlp.vocab)
+term = ["titel"]
+patterns = [nlp.make_doc(text) for text in term]
+print(patterns)
+matcher.add("titel", patterns)
+titel = None
+doc = nlp("Erstelle ein Termin um 15 Uhr mit dem x ich muss kacken")
+matches = matcher(doc)
+print(matches)
+
+for match_id, start, end in matches:
+    titel = doc[end:len(doc)]
+    print(titel.text)
+
+if not matches:
+    print("Keinen Titel gefunden was wollen Sie als Titel haben?")
+    #input bla bla into Titel
+
+def getTitel():
+    # Rule based wo man den alles nach "Mit dem Titel" als Titel nimmt, evtl Intent am Ende entfernen
+    # wenn kein titel gefunden, dann nochmal nachfragen was titel sein soll und das dann nutzen
+    # PHRASE Matcher anschauen, das wahrscheinlich sinnvoller
+    # TO:DO 
+    
+    pass

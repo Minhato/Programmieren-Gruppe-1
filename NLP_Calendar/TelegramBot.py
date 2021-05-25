1205583e86e6cd504d7ecebf2454d778aa7d5281
Minh <Minh-Huynh@outlook.de>
2021-05-25 18:53:20 +0200

    Telegram paar Methoden

--- NLP_Calendar/TelegramBot.py
+++ NLP_Calendar/TelegramBot.py
@@ -1,1 +1,20 @@
-print("test")
+import telebot
+import spacy
+
+#TheBotler
+tokenAPI = "1816801935:AAHAH98soREBBJvN2MQOAT4FaaCByDevG9w"
+
+bot = telebot.TeleBot(tokenAPI)
+@bot.message_handler(commands=['start'])
+def echo_message(message):
+    chat_id = message.chat.id    
+    bot.send_message(chat_id, "Der beste Botler")
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    chat_id = message.chat.id
+    eingabe = message.text
+    result = "yeet"
+    bot.send_message(chat_id, result)
+
+bot.polling

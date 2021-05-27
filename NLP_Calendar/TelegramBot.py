5c5b54e9092e2e942a59e64575598148578a8536
Minh <Minh-Huynh@outlook.de>
2021-05-27 21:09:20 +0200

    telegrambot polling

--- NLP_Calendar/TelegramBot.py
+++ NLP_Calendar/TelegramBot.py
@@ -2,13 +2,15 @@ import telebot
 import spacy
 
 #TheBotler
-tokenAPI = "1816801935:AAHAH98soREBBJvN2MQOAT4FaaCByDevG9w"
+tokenAPI = '1816801935:AAHAH98soREBBJvN2MQOAT4FaaCByDevG9w'
 
 bot = telebot.TeleBot(tokenAPI)
 @bot.message_handler(commands=['start'])
 def echo_message(message):
     chat_id = message.chat.id    
     bot.send_message(chat_id, "Der beste Botler")
+    print("stratet")
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
@@ -16,5 +18,6 @@ def echo_message(message):
     eingabe = message.text
     result = "yeet"
     bot.send_message(chat_id, result)
+    print("Test")
 
-bot.polling
+bot.polling()
